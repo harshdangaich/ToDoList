9aaa02c5c00e29402cb8015642f625c4408ea8f6
Harsh <135136807+harshdangaich@users.noreply.github.com>
2025-02-19 00:42:51 +0530

    Update db.js

--- TodoFree/server/config/db.js
+++ TodoFree/server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
  const connectDB=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://todolist:Harsh%4012@cluster0.ssfkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://todolist:Harsh%40123@cluster0.ssfkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("database connected")
     } catch (error) {
      console.error("The Error : "+error);
